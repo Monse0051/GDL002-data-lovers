6eddf3b2a546ea2e643cfdf7d294d9651be26194
Monse0051 <rouge0051@gmail.com>
2019-02-17 14:41:06 -0600

    Prueba 1 para subir cambios a mi rama de desarrollo, boton funcionando

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function next (){
+  document.getElementById("displayExplanation").style.display="none";
+  document.getElementById("displayPokemonList").style.display = "block";
+
+
+ }
+
+document.getElementById("continueButton").addEventListener("click", next);
