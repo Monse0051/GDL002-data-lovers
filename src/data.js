a89b851801ea3d9f274cdaa573822cbaccc08536
Monse0051 <rouge0051@gmail.com>
2019-02-20 08:33:57 -0600

    FUNCIONES
      - Creamos objeto windou.dataLovers.
      - Creamos la función filterByType para filtrar pokemones por tipo.
      - Creamos funcón average para calcular el promedio de caramelos para
        evolucionar pokemones.

--- src/data.js
+++ src/data.js
@@ -1,8 +1,36 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+
+window.dataLovers = {
+
+/*
+* filters pokemon Type
+* param pokemon: is a list of pokemon objects
+* param type: string type ("Grass, Poison, ...")
+*/
+
+filterByType (pokemon, type) {
+  const filterType = [];
+  for(let i= 0; i < pokemon.length; i++){
+    for(let t=0; t < pokemon[i].type.length; t++){
+      if (pokemon[i].type[t] === type){
+        filterType.push(pokemon[i]);
+      }
+    }
+  }
+
+  return filterType;
+},
+
+// promediar
+average () {
+  let pokemons = POKEMON.pokemon;
+  let contador = 0;
+  for (let i=0; i<pokemons.length; i++){
+    if (pokemons[i].hasOwnProperty('candy_count')) {
+      contador += pokemons[i].candy_count;
+    }
+  }
+  return contador / 70;
+  }
+
 };
-
-window.example = example;
