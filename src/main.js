ab275f5d14977d206d6a633fa7956ca5bd7d2888
Monse0051 <rouge0051@gmail.com>
2019-02-20 08:48:07 -0600

    FUNCIONES DEL DOM
      - Creamos funcion para el primer cambio de pantalla que escucha al
        boton de continuar.
      - Creamos funciones para meter los pokemones al HTML de manera
        dinamica y se visualicen en pantalla.
      - Creamos funcion para limpiar pantalla y filtrar de manera dinamica
        los pokemones por tipo.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-function next (){
+function next(){
   document.getElementById("displayExplanation").style.display="none";
   document.getElementById("displayPokemonList").style.display = "block";
-
-
  }
 
 document.getElementById("continueButton").addEventListener("click", next);
+
+function fillElements (pokemonList, divElement){
+  for(let i=0; i<pokemonList.length; i++) {
+    let divPokemon = document.createElement("div");
+    divPokemon.className = "pokemon";
+    divPokemon.innerHTML = "<img src=\"" +pokemonList[i].img + "\">";
+    divElement.insertAdjacentElement("beforeend", divPokemon);
+  }
+}
+
+function showPokemons(){
+  let pokemonList = POKEMON.pokemon;
+  let divPokemonList = document.getElementById("listOfPokemons");
+  fillElements(pokemonList, divPokemonList);
+}
+
+document.getElementById("continueButton").addEventListener("click", showPokemons);
+
+function filterPokemons(type){
+  let pokemonList = POKEMON.pokemon;
+  let divPokemonList = document.getElementById("listOfPokemons");
+  //clear pokemons
+  divPokemonList.innerHTML = "";
+  let filteredPokemons = window.dataLovers.filterByType(pokemonList, type);
+
+  fillElements(filteredPokemons, divPokemonList);
+}
